04c65f358758616a62dd103845c8676f8d372ed6
farmerdv <farmerdv@gmail.com>
2020-10-13 11:35:15 +0900

    1st commit

---
+++ src/main/java/mall/external/Cancellation.java
@@ -0,0 +1,28 @@
+package mall.external;
+
+public class Cancellation {
+
+    private Long id;
+    private Long oderId;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOderId() {
+        return oderId;
+    }
+    public void setOderId(Long oderId) {
+        this.oderId = oderId;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
